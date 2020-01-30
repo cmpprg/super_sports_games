27653c1ebc5baadb01761d359d7259fd52397803
Ryan Camp <cmpprg@icloud.com>
2020-01-30 14:30:22 -0700

    set attr reader for ages variable

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,10 @@
 require "./lib/standard_deviation"
 
 class Event
-  attr_reader :name
+  attr_reader :name :ages
   def initialize(sport_name, ages_array)
     @name = sport_name
+    @ages = ages_array
   end
 
 end
