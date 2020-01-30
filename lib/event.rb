2dcd28b60c6eca89c5be3ae6ced8550f4d646bfc
Ryan Camp <cmpprg@icloud.com>
2020-01-30 14:34:18 -0700

    Make max age method

--- lib/event.rb
+++ lib/event.rb
@@ -1,10 +1,16 @@
 require "./lib/standard_deviation"
 
 class Event
-  attr_reader :name :ages
+  attr_reader :name, :ages
+
   def initialize(sport_name, ages_array)
     @name = sport_name
     @ages = ages_array
   end
+
+  def max_age
+    @ages.max
+  end
+
 
 end
