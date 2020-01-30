71aff3a0a7ca42e3eb1fb27715fd86ca9b30ab82
Ryan Camp <cmpprg@icloud.com>
2020-01-30 13:37:20 -0700

    Make standard deviation method

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,13 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+def standard_deviation(set)
+  mean = set.sum / set.length.to_f
+  new_set = []
+  set.each do |num|
+    new_set << (num - mean)**2
+  end
+  new_mean = new_set.sum / set.length
+  Math.sqrt(new_mean).round(2)
+end
 
-# When you find the standard deviation, print it out
+puts standard_deviation(ages)
